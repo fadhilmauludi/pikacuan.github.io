e224dedfbd21776c662a25e93495d84600cba184
xenix <116736704+fadhilmauludi@users.noreply.github.com>
2025-11-24 09:43:56 +0000

    fix errror blank page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { HashRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
