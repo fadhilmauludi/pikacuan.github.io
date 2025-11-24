1d087a8fadb1965bc9e37c28156adc28075df6f2
xenix <116736704+fadhilmauludi@users.noreply.github.com>
2025-11-24 09:49:30 +0000

    fix errror blank page

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/pikacuan.online/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
